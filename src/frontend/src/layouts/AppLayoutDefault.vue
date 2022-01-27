740adec14d393d2aa7d633d0caaf5c8102c3b6b9
milickij <milickij@gmail.com>
2022-01-27 21:10:10 +0300

    Добавляет страницу корзины

--- src/frontend/src/layouts/AppLayoutDefault.vue
+++ src/frontend/src/layouts/AppLayoutDefault.vue
@@ -1,15 +1,19 @@
 <template>
   <div>
-    <app-layout-header />
+    <app-layout-header :totalPrice="getTotalPrice" />
     <slot />
   </div>
 </template>
 
 <script>
 import AppLayoutHeader from "./AppLayoutHeader.vue";
+import { mapGetters } from "vuex";
 
 export default {
   name: "AppLayoutDefault",
   components: { AppLayoutHeader },
+  computed: {
+    ...mapGetters("Cart", ["getTotalPrice"]),
+  },
 };
 </script>
